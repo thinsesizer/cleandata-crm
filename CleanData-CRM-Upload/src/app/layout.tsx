beeaec5ee45b59b3210fc11b83a59d3517a41322
thinsesizer <portos69@hotmail.com>
2026-03-19 13:48:49 +1100

    Add files via upload

--- CleanData-CRM-Upload/src/app/layout.tsx
+++ CleanData-CRM-Upload/src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Geist } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'sonner'
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body className={inter.className}>
         {children}
         <Toaster position="top-right" />
